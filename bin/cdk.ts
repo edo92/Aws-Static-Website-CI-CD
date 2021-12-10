e49d7651d0c06d44a3f3bd74e85a73adcd419fe8
Eduard Jacobs <edoter92@gmail.com>
2021-12-10 02:11:17 -0800

    config

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
+import 'tsconfig-paths/register';
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { Infrastructure } from '@stack';
+import { Infrastructure } from '../lib/stack';
 
 const app = new cdk.App();
 new Infrastructure(app, 'App-Infrastrucuture');
