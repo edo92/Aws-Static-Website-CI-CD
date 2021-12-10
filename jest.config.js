6a1d7a7ecacaf56d058f7fd3fc41d5b0addc3425
Eduard Jacobs <edoter92@gmail.com>
2021-12-10 11:31:26 -0800

    test env setup

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
+const path = require('path');
+
 module.exports = {
+   preset: 'ts-jest',
    testEnvironment: 'node',
-   roots: ['./test'],
-   testMatch: ['**/*.test.ts'],
-   transform: {
-      '^.+\\.tsx?$': 'ts-jest',
+   moduleNameMapper: {
+      '^@construct/(.*)$': path.resolve(__dirname, './lib/construct/$1'),
+      '^@pattern/(.*)$': path.resolve(__dirname, './lib/pattern/$1'),
+      '^@stack/(.*)$': path.resolve(__dirname, './lib/stack/$1'),
+      '^@config': path.resolve(__dirname, './config.json'),
    },
 };
